ce11fc30faae8be86bf7192e2c6346adfd4a9c6e
wtfzambo <zambelli.federico@gmail.com>
2023-04-23 22:01:41 +0200

    feat: Add terraform files

---
+++ infra/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_version = ">= 1.0"
+  backend "local" {}
+  required_providers {
+    google = {
+        source = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project
+  region = var.region
+}
+
+resource "google_bigquery_dataset" "subreddit_data_raw" {
+  dataset_id = var.raw_dataset
+  project = var.project
+  location = var.region
+
+  delete_contents_on_destroy = true
+}
+
+resource "google_bigquery_table" "submissions" {
+  table_id = var.submissions_table
+  dataset_id = google_bigquery_dataset.subreddit_data_raw.dataset_id
+  project = var.project
+
+  deletion_protection = false
+}
+
+resource "google_bigquery_table" "comments" {
+  table_id = var.comments_table
+  dataset_id = google_bigquery_dataset.subreddit_data_raw.dataset_id
+  project = var.project
+
+  deletion_protection = false
+}
---
+++ infra/variables.tf
@@ -0,0 +1,27 @@
+variable "project" {
+  description = "Your GCP project ID"
+}
+
+variable "region" {
+  description = "Region for GCP resources."
+  default = "europe-west6"
+  type = string
+}
+
+variable "raw_dataset" {
+  description = "BigQuery dataset that raw data will be written to."
+  type = string
+  default = "subreddit_data_raw"
+}
+
+variable "submissions_table" {
+  description = "Table name for the submissions' data."
+  type = string
+  default = "submissions"
+}
+
+variable "comments_table" {
+  description = "Table name for the comments' data."
+  type = string
+  default = "comments"
+}
